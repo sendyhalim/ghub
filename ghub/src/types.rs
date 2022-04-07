4ba974653826cf3217c1ea62905ef4115070e205
Sendy Halim <sendyhalim93@gmail.com>
2022-04-08 00:56:06 +0700

    refactor: Replace failure with anyhow

--- ghub/src/types.rs
+++ ghub/src/types.rs
@@ -1,1 +1,1 @@
-pub type ResultDynError<T> = Result<T, failure::Error>;
+pub type ResultDynError<T> = anyhow::Result<T>;
--- ghub/src/v3/util.rs
+++ ghub/src/v3/util.rs
@@ -1,3 +1,5 @@
+use anyhow::anyhow;
+use anyhow::Error;
 use reqwest::Response;
 use serde_json::Value;
 
@@ -9,19 +11,19 @@ pub(crate) fn api_path(api_path: &str) -> String {
 
 pub(crate) async fn result_from_server_response(res: Response) -> ResultDynError<Value> {
   if res.error_for_status_ref().is_err() {
-    let res_body: Value = res.json().await.map_err(failure::Error::from)?;
+    let res_body: Value = res.json().await.map_err(Error::from)?;
     let err_msg: String = res_body["errors"][0]["message"]
       .as_str()
-      .ok_or(failure::format_err!(
+      .ok_or(anyhow!(
         "Could not read github error message. Response body {}",
         serde_json::to_string(&res_body)?
       ))?
       .to_owned();
 
-    return Err(failure::err_msg(err_msg));
+    return Err(anyhow!(err_msg));
   }
 
-  let res_body = res.json().await.map_err(failure::Error::from)?;
+  let res_body = res.json().await.map_err(Error::from)?;
 
   return Ok(res_body);
 }
